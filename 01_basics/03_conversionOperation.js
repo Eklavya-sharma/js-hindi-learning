29ca1f8bbe7bb77791f4fe7101dcc59b8e0063d8
Eklavya-sharma <146164799+Eklavya-sharma@users.noreply.github.com>
2023-12-29 18:16:47 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = 33
+
+// console.log(typeof score);=>number
+// console.log(typeof(score));=>number
+
+//let score = "33"
+
+//console.log(typeof score);//=>string
+//console.log(typeof(score));//=>string
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)//=>number
+//console.log(valueInNumber);//=>33
+ //"33abc" =>  string,string,number,NaN
+ //null => object,object,number,0
+ //undefined => undefined,undefined,nuumber,NaN
+ //true => boolean,boolean,number,1
+ //"suman"=> string,string, number,NaN
+
+ //let isLoggedIn = "suuman"
+ //let booleanLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanLoggedIn)//=>true;
+//""=> false
+//1 => true
+//"suman"=>true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);//=>33
+console.log(typeof stringNumber);//=>string
+
+
+
+
