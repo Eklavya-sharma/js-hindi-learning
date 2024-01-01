9bd10a52f267578b9a1de93e44dbabd94c39e38e
Eklavya-sharma <146164799+Eklavya-sharma@users.noreply.github.com>
2024-01-01 06:56:27 +0000

    comparsionDatatypes

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,11 @@
+console.log("2" > 1);//true only check numbers and datatype easily convert
+console.log("02" > 1);//true
+console.log(null > 0);//false its very diffcult to check
+console.log(null == 0);//false
+console.log(null >= 0);//here null convert to zero thats why is true
+console.log(undefined > 0);//false
+console.log(undefined == 0);//false
+console.log(undefined >= 0);//false
+//strict check
+//====
+console.log("2" === 2);//here bith check numbers and datatype
