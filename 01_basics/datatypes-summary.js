0f7a75b633ad943d669403fd6a145d9b4f6cd364
Eklavya-sharma <146164799+Eklavya-sharma@users.noreply.github.com>
2024-01-01 07:24:02 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// DATA TYPES
+// primitive type
+// 7 categories : string, number, boolean, null, undefined, Symbol, BigInt
+//"************************************************************"
+// refrence (Non primitive)
+//Array, Objects , Functions
+
+
+console.log("SUMAN SOURAV");
+let name = "suman";
+let age = 22;
+let isLoggedIn =false;
+let state;
+
+const id = Symbol('1233');
+const anotherId = Symbol('1233');
+
+console.log(id === anotherId);
+
+
+const heroes = ["shaktiman","doga","nagaraj"]
+console.log(heroes);
+let myObj={
+    Name:"hitesh",
+    Sage: 44,
+}
+console.log(myObj);
+ const myfunction = function(){
+    console.log("helloworld")
+ }
+ console.log(typeof myObj);
