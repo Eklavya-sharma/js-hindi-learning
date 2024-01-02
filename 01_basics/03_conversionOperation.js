556f40c71f37ed51085c82e5839528b5d075e950
Eklavya-sharma <146164799+Eklavya-sharma@users.noreply.github.com>
2024-01-02 10:32:01 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -59,6 +59,109 @@ console.log("**********************************************")
 console.log("**********************************************")
 console.log(+true)//=>1
 console.log(+"")=>0
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);//=>33
+// console.log(typeof stringNumber);//=>string
+//**************************************** OPERATION ************************************************//
+//  let value = 3
+//  let negValue = -value
+//  console.log(negValue)//=>-3
+console.log ("*******************ARITHMETIC-OPERATOR**********************")
+ let a = 3
+ let b= 4
+ console.log("a + b =",a+b)//=>7
+ console.log("a - b =",a-b)//=>-1
+ console.log("a / b =",a/b)//=>0.75
+ console.log("a % b =",a%b)//=>3
+ console.log("a ** b=",a**b)//=>81
+ console.log("a * b =",a*b)//=>>12
+ console.log("******************************************")
+
+ let str1 = "suman "
+ let str2 = "sourav "
+ let str3 = str1 + str2
+ console.log(str3)
+console.log("**********************************************")
+//  console.log("1"+2);//=>12
+//  console.log(1+"2");//=>12
+//  console.log("1"+"2");//=>12
+//  console.log("1"+2+2);//=>122
+//  console.log(1+2+"2");//=>32
+
+console.log("**********************************************")
+console.log("***************************URANARY-OPERATOR*****************************")
+console.log("******POST-INCREMENT************")
+let c=5
+console.log("c=",c)
+console.log("c++ =",c++)
+console.log("c =",c)
+console.log("*************PRE-INCREMENT*************")
+let d = 6
+console.log("d =",d)
+console.log("++d =",++d)
+console.log("d =",d)
+console.log("*************************************")
+
+console.log("*******************ASSIGNMENT-OPERATOR = , + =, - =, % =, ** =*****************")
+
+let e = 4
+console.log("e=",e)
+e+=4
+console.log("e =",e)
+let f = 7
+console.log("f =",f)
+f-=2
+console.log("f =",f)
+console.log("***************************************************************************")
+
+console.log("***************COMPARSION-OPERAATOR*****************************************")
+console.log("equal ==, equal to & datatype ===, not equal !=, not equal to datatype !==")
+
+let h=5
+let i=5
+console.log("h = = i",h == i)
+let j="5"
+console.log("h === j",h === j)
+
+console.log("*************LOGICAL-OPERATOR************************************")
+
+console.log("****** AND OPERATOR *******")
+
+let a1=6
+let b1=6
+
+let cond1 = a1 >= b1
+let cond2 = a1 === b1
+console.log(" cond1 && cond2 =", cond1 && cond2)
+
+console.log("******OR OPERATOR *********")
+let a2=7
+let b2=6
+
+let cond3 = a2 > b2
+let cond4 = a2 < b2
+console.log(" cond3 && cond4 ",cond3 || cond4)
+console.log("*******************************")
+
+console.log("************* TERNARY-OPERATOR ***********")
+console.log(" syntax of terenary operator ==== condition ? true output: false-output")
+ 
+console.log("==============================================================")
+
+let age = 25
+let result = age >= 18 ? " adult ": " not adult"
+console.log( result)
+
+
+
+
+
+
+
+
+
  
 
 
